056acf5706fec554ea27965ff54b50bff0df944e
Artem <karimov.art89@gmail.com>
2024-03-14 20:08:05 +0300

    Initial commit

---
+++ main_without_data.py
@@ -0,0 +1,95 @@
+from pprint import pprint
+
+import requests
+import json
+
+
+def get_photos(photos_list, count):
+    all_photos = photos_list['response']['items']
+
+    photos_name_list = []
+
+    likes_list = []
+
+    for item in all_photos:
+        if len(photos_name_list) == max_photos:
+            break
+
+        photo_params = {}
+        likes = item['likes']['count']
+        if likes in likes_list:
+            file_name = f'{likes}_{item['date']}.jpg'
+        else:
+            file_name = f'{likes}.jpg'
+
+        size_params = item['sizes'][-1]
+
+        photo_params['file_name'] = file_name
+        photo_params['size'] = size_params['type']
+        photo_params['url'] = size_params['url']
+
+        photos_name_list.append(photo_params)
+        likes_list.append(likes)
+
+    return photos_name_list
+
+
+def create_log_file(data):
+    with open('log.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(data, indent=2))
+
+
+class VK:
+    BASE_URL = 'https://api.vk.com/method/'
+
+    def __init__(self, access_token, user_id, version='5.199'):
+        self.token = access_token
+        self.id = f'id{user_id}'
+        self.version = version
+        self.params = {'access_token': self.token, 'v': self.version}
+
+    def get_photos(self, owner_id, album_id):
+        params = {'user_ids': self.id, 'owner_id': owner_id, 'album_id': album_id, 'extended': 1}
+        params.update(self.params)
+        response = requests.get(f'{self.BASE_URL}photos.get', params=params)
+        return response.json()
+
+
+class YD:
+    BASE_URL = 'https://cloud-api.yandex.net'
+
+    def __init__(self, y_token):
+        self.token = y_token
+        self.headers = {'Authorization': f'OAuth {y_token}'}
+
+    def create_dir(self, dir_name):
+        params = {'path': f'{dir_name}'}
+        response = requests.put(f'{self.BASE_URL}/v1/disk/resources', params=params, headers=self.headers)
+
+    def photos_backup(self, photos_list):
+        dir_name = '123'
+        self.create_dir(dir_name)
+
+        for photo in photos_list:
+            params = {'path': f'{dir_name}/{photo['file_name']}',
+                      'overwrite': True,
+                      'url': photo.pop('url')}
+            response = requests.post(f'{self.BASE_URL}/v1/disk/resources/upload',
+                                     params=params,
+                                     headers=self.headers)
+        create_log_file(photos_list)
+
+
+if __name__ == '__main__':
+    access_token = ("07938e6207938e6207938e62560484ee5b0079307938e62624b66156e4ac4263db5abf6")  # сервисный ключ
+    user_id =   # идентификатор пользователя VK
+    y_token = '' # токен с полигона Yandex
+
+    vk = VK(access_token, user_id)
+    all_photos = vk.get_photos(user_id, 'profile')
+
+    max_photos = 5
+    photos_list = get_photos(all_photos, max_photos)
+
+    y_disk = YD(y_token)
+    y_disk.photos_backup(photos_list)
